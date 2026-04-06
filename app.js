3b09b9dbb2f15db47f6025ac0e6ac1898b7d8a26
Bagio1 <mohamedfouhamy26@gmail.com>
2026-04-07 01:26:42 +0400

    initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+  res.status(200).json({ message: 'Hello from Bagio' });
+});
+
+module.exports = app;
---
+++ app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('./app');
+
+describe('GET /api/hello', () => {
+  it('should return status 200', async () => {
+    const res = await request(app).get('/api/hello');
+    expect(res.statusCode).toBe(200);
+  });
+});
