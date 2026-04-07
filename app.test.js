d60de90cf39e5ee030028c0a395c8e1c6590d79f
Bagio1 <mohamedfouhamy26@gmail.com>
2026-04-07 09:06:25 +0400

    Add unit tests

--- app.test.js
+++ app.test.js
@@ -1,10 +1,26 @@
 const request = require('supertest');
 const app = require('./app');
 
+
+//imtergration test
 describe('GET /api/hello', () => {
   it('should return status 200', async () => {
     const res = await request(app).get('/api/hello');
     expect(res.statusCode).toBe(200);
+  });
+
+
+
+//unit test 
+
+  it('return JSON content-type', async () => {
+    const res = await request(app).get('/api/hello');
+    expect(res.headers['content-type']).toMatch(/json/);
+  });
+
+  it('return a message field in the body', async () => {
+    const res = await request(app).get('/api/hello');
+    expect(res.body).toHaveProperty('message');
   });
 });
     
